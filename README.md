b638c1a010cd68d91822afa8942b87501b01352e
harsh <mh.harsh@gmail.com>
2016-12-23 13:30:30 +0530

    adding gitignore readme and adding burrow repo

---
+++ README.md
@@ -0,0 +1,8 @@
+This repository configures two zookeeper containers and two kafka containers .
+The whole setup is build on the following versions:
+
+ansible version:2.2.0.0
+docker version: Docker version 1.12.3, build 6b644ec
+
+The entry point for the script is run.sh located in the main directory .
+Execute : `sh run.sh` to get the environment up and running.
--- run.sh
+++ run.sh
@@ -3,14 +3,20 @@
 echo "Removing  the existing image"
 docker-compose down
 
-echo "Copying public key to current directory..."
-cp ~/.ssh/id_rsa.pub .
+# echo "Copying public key to current directory..."
+# cp ~/.ssh/id_rsa.pub .
 
-echo "Force delete the existing image"
-docker rmi -f harsh/ubuntu-ssh1
+# echo "Force delete the existing java image"
+# docker rmi -f harsh/ubuntu-java-ssh
 
-echo "Building the docker image..."
-docker build -t harsh/ubuntu-ssh1 . 
+# echo "Force delete the existing golang image"
+# docker rmi -f harsh/ubuntu-go-ssh
+
+# echo "Building the docker image for java..."
+# docker build -t harsh/ubuntu-java-ssh -f Dockerfile_java .
+
+# echo "Building the docker image for golang..."
+# docker build -t harsh/ubuntu-go-ssh -f Dockerfile_go .
 
 echo "Bringing the kafka cluster up ..."
 docker-compose up -d 
